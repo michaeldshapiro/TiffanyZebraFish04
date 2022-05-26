9cb9c7322864a25dde725a2dcff9de58cf77765a
Michael Shapiro <michael.shapiro@crick.ac.uk>
2022-05-26 11:13:01 +0100

    Made new objects matching the available cells in version 2 combined.

---
+++ makeCopyQCFigures.R
@@ -0,0 +1,47 @@
+
+library(Seurat)
+library(HandyPack)
+library(plotly)
+library(ggplot2)
+
+rm(list=ls())
+graphics.off()
+
+## ####################################################
+## ####################################################
+files = Sys.glob('intermediateCopyQC/*Cell*')
+
+saveDir = nameAndMakeDir('copyQCFigures')
+vars = c('UMAP_1','UMAP_2','orig.ident','CellType')
+for(file in files)
+{
+    f = readRDS(file)
+    df = FetchData(f,vars)
+    for(var in c('orig.ident','CellType'))
+    {
+        N = length(unique(df[,var]))
+        colors = polychrome(N)
+        title = str_replace(file,'intermediateCopyQC/','')
+        title = str_replace(title,'.rds','')
+        title = paste(var,'in',title)
+        g = ggplot(df,aes_string(x='UMAP_1',y='UMAP_2',color=var)) +
+            geom_point() +
+            scale_color_manual(values=colors) +
+            ggtitle(title)
+
+        ## dev.new()
+        ## print(g)
+        fileName = paste0('copyQCFigures/',
+                          title,
+                          '.jpg')
+        ggsave(plot=g,
+               filename=fileName,
+               width=14,height=9,units='in')
+
+        p = ggplotly(g)
+        print(p)
+        fileName = str_replace(fileName,'jpg','html')
+        htmlwidgets::saveWidget(as_widget(p), fileName)
+    }
+}
+    
